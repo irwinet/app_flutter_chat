1f34915bc15ffa4e8f99e28833e442416ca119d9
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-13 16:11:44 -0500

    Advanced users page

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,14 +1,60 @@
+import 'package:app_flutter_chat/models/usuario.dart';
 import 'package:flutter/material.dart';
 
 
-class UsuariosPage extends StatelessWidget {
+class UsuariosPage extends StatefulWidget {
+
+  @override
+  State<UsuariosPage> createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+  final usuarios = [
+    Usuario(online: true, email: 'test1@test.com', nombre: 'Maria', uid: '1'),
+    Usuario(online: true, email: 'test2@test.com', nombre: 'Melissa', uid: '2'),
+    Usuario(online: true, email: 'test3@test.com', nombre: 'Fernando', uid: '3'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('UsuariosPage'),
-     ),
-   );
+      appBar: AppBar(
+        title: const Text('Mi Nombre', style: TextStyle(color: Colors.black87),),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+
+          }, 
+          icon: Icon(Icons.exit_to_app, color: Colors.black87,)
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            // child: Icon(Icons.check_circle, color: Colors.blue[400],),
+            child: Icon(Icons.offline_bolt, color: Colors.red,),
+          )
+        ],
+      ),
+      body: ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: ( _, i) => ListTile(
+          title: Text(usuarios[i].nombre),
+          leading: CircleAvatar(
+            child: Text(usuarios[i].nombre.substring(0,2)),
+          ),
+          trailing: Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(
+              color: usuarios[i].online ? Colors.green[300] : Colors.red,
+              borderRadius: BorderRadius.circular(100)
+            ),
+          ),
+        ), 
+        separatorBuilder: ( _, i) => Divider(), 
+        itemCount: usuarios.length
+      ),
+    );
   }
 }
